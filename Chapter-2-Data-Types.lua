6ff90a292cbfe095030f3027ee8ba475cb8ce80a
miabhishekgaikwad <abhishekgaikwad065@gmail.com>
2026-03-17 13:12:23 +0530

    chore: add additional comments and structure to Chapter 2: Data Types

--- Chapter-2-Data-Types.lua
+++ Chapter-2-Data-Types.lua
@@ -2,3 +2,23 @@
 
 
 
+local playerName = "Rohit"  -- string
+local playerMoney = 5000    -- number
+local isPlayerAlive = false -- boolean
+local playerJob = "job"     -- nil (job assigned nahi hai)
+
+
+-- print("Player Money: " .. type(playerMoney))
+-- print("Player Name: " .. type(playerName))
+-- print("Player Alive: " .. type(isPlayerAlive))
+-- print("Player Job: " .. type(playerJob))
+
+
+
+-- Agar playerJob == nil → "No Job"
+-- warna → "Job Assigned"
+-- if playerJob ~= nil then
+--     print("Job Assigned")
+-- else
+--     print("No Job")
+-- end
