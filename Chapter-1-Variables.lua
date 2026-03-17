626cfa299ad9b7a70bfb657d9e47af197ae550c5
miabhishekgaikwad <abhishekgaikwad065@gmail.com>
2026-03-17 12:22:35 +0530

    refactor variable names for clarity and consistency

--- Chapter-1-Variables.lua
+++ Chapter-1-Variables.lua
@@ -1,11 +1,23 @@
-local player = "Abhishek"
-local money = 5000
+local playerName = "Abhishek"
+local playerMoney = 5000
 local playerJob = "Trucker"
 local playerLevel = 5
 local playerAlive = true
 
-print("Player Name: " .. player)
-print("Player Money: " .. money)
+print("Player Name: " .. playerName)
+print("Player Money: " .. playerMoney)
 print("Player Job: " .. playerJob)
 print("Player Level: " .. playerLevel)
-print("Player IsAlive: " ..  tostring( playerAlive))
+print("Player IsAlive: " .. tostring(playerAlive))
+
+
+
+-- Agar playerMoney > 1000 → print "Rich Player"
+-- warna → print "Poor Player"
+
+if playerMoney and playerMoney == 'number' then
+elseif playerMoney > 100 then
+    print('Rich Player')
+else
+    print('Poor Player')
+end
