f6368d9506aaaa75cf781067f3c5881d4c657e50
miabhishekgaikwad <abhishekgaikwad065@gmail.com>
2026-03-17 12:08:46 +0530

    first commit

---
+++ Chapter-1-Variables.lua
@@ -0,0 +1,11 @@
+local player = "Abhishek"
+local money = 5000
+local playerJob = "Trucker"
+local playerLevel = 5
+local playerAlive = true
+
+print("Player Name: " .. player)
+print("Player Money: " .. money)
+print("Player Job: " .. playerJob)
+print("Player Level: " .. playerLevel)
+print("Player IsAlive: " .. tostring(playerAlive))
