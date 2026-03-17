cc01cf9a7bba494ca85adbb0140a007ee83bdd7e
miabhishekgaikwad <abhishekgaikwad065@gmail.com>
2026-03-17 20:20:18 +0530

    chore: comment out canEnterArea function and related print statement for future reference

--- Chapter-4-Comparison-Operators.lua
+++ Chapter-4-Comparison-Operators.lua
@@ -23,33 +23,53 @@
 -- requiredLevel > 100 → exploit
 
 
-local function canEnterArea(level, requiredLevel)
-    if type(level) ~= "number" then
-        return false
-    end
-    
-    if  type(requiredLevel) ~= "number" then
-        return false
-        
-    end
-    if  requiredLevel < 0 then
-        return false
-        
-    end
-    if  level < 0 then
-        return false
-        
-    end
+-- local function canEnterArea(level, requiredLevel)
+--     if type(level) ~= "number" or type(requiredLevel) ~= "number" then
+--         return false, "Invalid Data"
+--     end
 
-    if level >= requiredLevel then
-        return true
-    else
-        return false
-    end
 
-end
+--     if requiredLevel < 0 or level < 0 then
+--         return false, "Exploit Detected"
+--     end
 
+
+--     if requiredLevel > 100 or level > 100 then
+--         return false, "Exploit Detected"
+--     end
+
+
+--     if level == requiredLevel then
+--         return true, "Exact Level Match"
+--     elseif level >= requiredLevel then
+--         return true, "Access Granted"
+--     else
+--         return false, "Level Too Low"
+--     end
+-- end
 
 
 
-print(canEnterArea(1,-10))
+-- print(canEnterArea(10, 10))
+
+
+
+-- STEP 7 — 🧪 Practice Challenge
+
+-- 👉 Ek function likho:
+
+-- function name: canStartJob
+
+-- Conditions:
+
+-- player level ≥ 5
+
+-- player ke paas job license ho
+
+-- player banned na ho
+
+-- 👉 Output:
+
+-- true → allowed
+
+-- false → denied
