76d78e2e5d4b75742763acbca754c52d5dbe80bb
miabhishekgaikwad <abhishekgaikwad065@gmail.com>
2026-03-17 13:38:12 +0530

    chore: clean up comments and formatting in Chapter 2: Data Types

--- Chapter-2-Data-Types.lua
+++ Chapter-2-Data-Types.lua
@@ -4,8 +4,9 @@
 
 local playerName = "Rohit"  -- string
 local playerMoney = 5000    -- number
+local playerLevel = 1       -- number
 local isPlayerAlive = false -- boolean
-local playerJob = nil        -- nil (job assigned nahi hai)
+local playerJob = nil       -- nil (job assigned nahi hai)
 
 
 -- print("Player Money: " .. type(playerMoney))
@@ -29,11 +30,33 @@ local playerJob = nil        -- nil (job assigned nahi hai)
 -- warna → "Unemployed"
 
 
-    -- if playerJob == "police" then
-    --     print("Player Job: Police Officer")
-    -- elseif playerJob == "ambulance" then
-    --     print("Player Job: Medic")
-    -- else
-    --     print("Player is a Unemployed")
-    -- end
+-- if playerJob == "police" then
+--     print("Player Job: Police Officer")
+-- elseif playerJob == "ambulance" then
+--     print("Player Job: Medic")
+-- else
+--     print("Player is a Unemployed")
+-- end
 
+
+
+-- Agar playerJob == "police" AND playerLevel >= 10 → "Senior Police"
+-- Agar playerJob == "police" → "Junior Police"
+-- warna → "Other Job"
+
+
+-- local job = string.lower(playerJob or '')
+-- if type(playerJob) ~= "string" then
+--     print("Player Job: Unemployed/Nill")
+--     return
+-- elseif job == "police" and playerLevel >= 10 then
+--     print("Player Job: Senior Police")
+-- else
+--     print("Player has Other Job")
+
+-- end
+
+
+-- Agar playerMoney > 10000 → "VIP Player"
+-- Agar playerMoney > 5000 → "Premium Player"
+-- warna → "Normal Player"
